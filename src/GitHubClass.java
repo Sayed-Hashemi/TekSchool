5d5777907fcc728857c6d43bfa04ec161acd642b
Sayed-Hashemi <mujtabahashemi4@gmail.com>
2022-05-12 16:48:06 -0700

    this is for lab practice

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -1,4 +1,25 @@
 
 public class GitHubClass {
-
+	
+	public static void main(String[]args ) {
+		
+//   	status: git status
+//		add one file : git add <fileName>
+//		add all files: git add . or git add *
+//		commit: git commit -m "message"
+//		push: git push -u origin <branchName>
+	
+	}
+	int a = 10;
+	int b = 10;
+	int c = a+b;
+	
+	{
+		
+	
+	System.out.println(c); 
+	}
+	
+	
+			
 }
